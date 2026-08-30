13ea1b2b8f9ba669e315b65a97001e9edccd230c
sanderossi <sanderossi@gmail.com>
2026-08-30 09:39:57 +0200

    Add central booking availability data

---
+++ data/bookings.js
@@ -0,0 +1,8 @@
+// Centrale lijst met bezette periodes voor Casa Náufragos.
+// arrival = eerste bezette nacht (inclusief)
+// departure = vertrekdag (exclusief, dus die dag kan een nieuwe gast aankomen)
+// Gebruik altijd ISO-datums: YYYY-MM-DD.
+export default [
+  // Voorbeeld (niet actief):
+  // { arrival: "2026-09-10", departure: "2026-09-17", note: "Reservering" },
+];
